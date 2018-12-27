a74b59d23de984cd6c325fa0f03469576772cb20
kter <me@kter.jp>
2018-12-27 23:31:07 +0900

    ユーザー情報キャッシュ

--- db/migrate/20181227142721_devise_create_users.rb
+++ db/migrate/20181227142721_devise_create_users.rb
@@ -32,6 +32,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
+      t.string :users, :provider
+      t.string :users, :uid
+      t.string :users, :token
+      t.string :users, :meta
 
       t.timestamps null: false
     end
