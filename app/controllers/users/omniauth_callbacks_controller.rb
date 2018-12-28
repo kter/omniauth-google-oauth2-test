9f6180a6a13aae458c1a402e9bac03cbcc11e3ad
kter <me@kter.jp>
2018-12-29 01:45:00 +0900

    ログイン後のデバッグ情報を追加

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,11 +1,10 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def google
-    logger.debug("Users::OmniauthCallbacksCOntroller.google")
     @user = User.find_for_google(request.env['omniauth.auth'])
 
     if @user.persisted?
       flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
-      sign_in_and_redirect @user, event: :authentication
+      sign_in_and_redirect :home_index_path, event: :authentication
     else
       session['devise.google_data'] = request.env['omniauth.auth']
       redirect_to new_user_registration_url
