ab535a6795e71c6370c07df75f66d231c9c2b65e
kter <me@kter.jp>
2018-12-28 12:41:06 +0900

    認証設定

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -287,4 +287,9 @@ Devise.setup do |config|
   # ActiveSupport.on_load(:devise_failure_app) do
   #   include Turbolinks::Controller
   # end
+  config.omniauth :google_oauth2,
+    ENV['GOOGLE_APP_ID'], # 環境変数に先ほど控えたクライアントIDを入れておく
+    ENV['GOOGLE_APP_SECRET'], # 環境変数に先ほど控えたシークレットを入れておく
+    name: :google,
+    scope: %w(email) 
 end
