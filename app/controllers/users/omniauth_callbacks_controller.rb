07ba957c661c728ab199f2560ebcc81cb6c913c0
kter <me@kter.jp>
2018-12-29 15:07:25 +0900

    ログイン後undefined method router_nameエラーとなる問題を解決

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,10 +4,14 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
     if @user.persisted?
       flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
-      sign_in_and_redirect :home_index_path, event: :authentication
+      sign_in_and_redirect @user, event: :authentication
     else
       session['devise.google_data'] = request.env['omniauth.auth']
       redirect_to new_user_registration_url
     end
+  end
+
+  def after_sign_in_path_for(resource)
+    home_index_path
   end
 end
